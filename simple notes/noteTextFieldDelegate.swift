204090d53bd4c93e7b725afc09cf2ebc1e6c02a0
Emerson M Luz <emerson.maciel@vortigo.com.br>
2022-11-15 13:47:28 -0300

    adicionado saveCache no return do keyboard virtual

--- simple notes/noteTextFieldDelegate.swift
+++ simple notes/noteTextFieldDelegate.swift
@@ -5,6 +5,7 @@ extension ViewController: UITextFieldDelegate {
     // Action para botão return do teclado virtual do Iphone
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         addNote()
+        saveCache()
         return true
     }
     

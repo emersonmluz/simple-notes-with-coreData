dc0010133fc25a5bb0ce0de0c448bfa7224aef59
Emerson M Luz <emerson.maciel@vortigo.com.br>
2022-11-15 11:16:25 -0300

    Colocado alerta antes de excluir nota

---
+++ simple notes/tableViewDelegate.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+extension ViewController: UITableViewDelegate {
+ 
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let trashNote =   UIContextualAction(style: .destructive, title: nil) { action, view, boolAction in
+        
+                tableView.performBatchUpdates {
+                    
+                    let alert = UIAlertController(title: "Confirme", message: "Deseja remover essa nota?", preferredStyle: .alert)
+                    
+                    alert.addAction(UIAlertAction(title: "Cancelar", style: .cancel))
+                    alert.addAction(UIAlertAction(title: "Remover", style: .destructive) {_ in
+                        self.notes.remove(at: indexPath.row)
+                        tableView.deleteRows(at: [indexPath], with: .left)
+                        self.effectDeleteNotes?.play()
+                        tableView.reloadData()
+                    })
+                    self.present(alert, animated: true)
+                }
+        
+                boolAction(true)
+            }
+            
+        trashNote.image = UIImage(systemName: "trash")
+        return UISwipeActionsConfiguration(actions: [trashNote])
+    }
+}
+
